7dd49ca50361d9f60780b22d497eca1f55cf31fa
SAMKIT <samkitjain4916@gmail.com>
2020-01-19 19:25:02 +0530

    final commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int x;
+    int i;
+
+    printf("please enter any number!\n");
+    scanf("%d",&x);
+    for(i=1;i<=10;i++)
+
+
+        {
+
+          printf("%d\n",x*i);   }
+
+    return 0;
+}
